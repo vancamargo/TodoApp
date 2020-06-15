52eab197d84644f03cf063f1899395d84fc0a915
vanessa camargo <vanessacamargo.v@gmail.com>
2020-06-15 18:15:11 -0300

    .

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Todo.Domain.Handlers;
 using Todo.Domain.Infra;
 using Todo.Domain.Infra.Contexts;
@@ -37,25 +39,44 @@ namespace Todo.Domaim.Api
             services.AddTransient<ITodoRepository, TodoRepository>();
             services.AddTransient<TodoHandler, TodoHandler>();
 
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.Authority = "https://securetoken.google.com/project-2422259968811979227";
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidIssuer = "https://securetoken.google.com/project-2422259968811979227",
+                       ValidateAudience = true,
+                       ValidAudience = "project-2422259968811979227",
+                       ValidateLifetime = true
+                   };
+               });
         }
+        //    var firebaseConfig = {
+        //apiKey: "AIzaSyCqdOo1BHHP74tSO1Eq7zdVt_B2lpRNpwI",
+        //authDomain: "project-2422259968811979227.firebaseapp.com",
+        //databaseURL: "https://project-2422259968811979227.firebaseio.com",
+        //projectId: "project-2422259968811979227",
+        //storageBucket: "project-2422259968811979227.appspot.com",
+        //messagingSenderId: "1092973353456",
+        //appId: "1:1092973353456:web:ade093d41eafdb46d8e535"
 
- 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseCors(x => x
                 .AllowAnyOrigin()
-                .AllowAnyHeader()
                 .AllowAnyMethod()
-            );
+                .AllowAnyHeader());
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -65,3 +86,6 @@ namespace Todo.Domaim.Api
         }
     }
 }
+
+
+
